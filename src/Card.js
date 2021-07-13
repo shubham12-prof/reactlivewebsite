1fd2156c42523009ba97d27a365c0b31ec130dc5
shubham12-prof <78645691+shubham12-prof@users.noreply.github.com>
2021-07-13 17:32:03 +0530

    MYSECONDCOMMIT

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import Aos from 'aos';
+import "aos/dist/aos.css";
 import Sdata from './Sdata';
 
 const Card = (props) => {
+    useEffect(() => {
+        Aos.init({duration:1000});
+    },[]);
     return (
         <>
             <div className="col-md-4 col-10 mx-auto">
-                <div class="card" >
-                    <img className="card-img-top" src={props.imgsrc}
+                <div class="card">
+                    <img  className="card-img-top"  data-aos="flip-left" data-aos-offset="200" src={props.imgsrc}
                         alt="Card image cap" />
-                    <div className="card-body">
+                    <div className="card-body" data-aos="fade-down">
                         <h5 className="card-title font-weight-bold">{props.title}</h5>
                         <p className="card-text">{props.write}</p>
                         <NavLink to=" " className="btn btn-primary">
--- src/Sdata.js
+++ src/Sdata.js
@@ -4,9 +4,39 @@ import fab from "./Images/M3.jpg"
 import mar from "./Images/M10.jpg"
 import apr from "./Images/M11.jpg"
 import may from "./Images/M12.jpg"
-import Quotes from "./Quotes"
 
 const Sdata= [
+    {
+        imgsrc:web,
+        title: "Beautiful",
+        write:"The mountains are the last place where man can feel truly wild."
+    },
+    {
+        imgsrc:jan,
+        title: "Dashing",
+        write:"My spirit soars where the air goes thin."
+    },
+    {
+        imgsrc:fab,
+        title: "stylish",
+        write:"The sun shines brightest from the peaks of mountains."
+        
+    },
+    {
+        imgsrc:mar,
+        title: "hot",
+        write:"Over every mountain, there is a pass, although it may not be seen from the valley"
+    },
+    {
+        imgsrc:apr,
+        title: "pretty",
+        write:"Mountains teach that not everything in this world can be rationally explained."
+    },
+    {
+        imgsrc:may,
+        title: "cool",
+        write:"“Chasing angels or fleeing demons, go to the mountains."
+    },
     {
         imgsrc:web,
         title: "Beautiful",
